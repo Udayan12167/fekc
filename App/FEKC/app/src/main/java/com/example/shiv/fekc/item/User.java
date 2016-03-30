c1c6e8f34021456bc05d98576b3a1df5e253a157
Udayan Tandon <udayan12167@iiitd.ac.in>
2016-03-30 10:49:31 +0530

    Add .gitignore

---
+++ App/FEKC/app/src/main/java/com/example/shiv/fekc/item/User.java
@@ -0,0 +1,26 @@
+package com.example.shiv.fekc.item;
+
+/**
+ * Created by shiv on 30/3/16.
+ */
+public class User {
+
+    private String name;
+    private String fbToken;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFbToken() {
+        return fbToken;
+    }
+
+    public void setFbToken(String fbToken) {
+        this.fbToken = fbToken;
+    }
+}
