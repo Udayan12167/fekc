912b4a900565e6aea02fd5553f7e40610dcbf970
Mrinal Kachhara <Mrinal Kachhara>
2016-03-31 01:21:52 +0530

    Display tasks completed with UI

--- App/FEKC/app/src/main/java/com/example/shiv/fekc/activity/ActivityBasedTaskActivity.java
+++ App/FEKC/app/src/main/java/com/example/shiv/fekc/activity/ActivityBasedTaskActivity.java
@@ -2,7 +2,6 @@ package com.example.shiv.fekc.activity;
 
 import android.app.DatePickerDialog;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -195,4 +194,6 @@ public class ActivityBasedTaskActivity extends AppCompatActivity {
 
 
     }
+
+
 }
