a734d28badb70381e7a52394f8059c6541a5ad3c
Udayan Tandon <udayan12167@iiitd.ac.in>
2016-04-04 16:52:53 +0530

    AppListActivity: Made UI changes

--- App/FEKC/app/src/main/java/com/example/shiv/fekc/activity/AppListActivity.java
+++ App/FEKC/app/src/main/java/com/example/shiv/fekc/activity/AppListActivity.java
@@ -1,19 +1,25 @@
 package com.example.shiv.fekc.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 
 import com.example.shiv.fekc.R;
@@ -30,9 +36,10 @@ public class AppListActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private AppListAdapter appListAdapter;
     private EditText searchEditText;
-    private LinearLayout searchViewLinearLayout;
     private ProgressBar progressBar;
-
+    private MenuItem mSearchAction;
+    private boolean isSearchOpened = false;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,11 +50,13 @@ public class AppListActivity extends AppCompatActivity {
             return;
         }
         packageManager = getPackageManager();
+        mToolbar = (Toolbar) findViewById(R.id.app_list_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Select Apps");
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
 
-        searchViewLinearLayout = (LinearLayout)findViewById(R.id.app_list_activity_linear_layout);
         recyclerView = (RecyclerView)findViewById(R.id.app_list_activity_recycler_view);
         recyclerView.setLayoutManager(linearLayoutManager);
         appListAdapter = new AppListAdapter(this, appItemList);
@@ -58,24 +67,6 @@ public class AppListActivity extends AppCompatActivity {
         progressBar = (ProgressBar)findViewById(R.id.activity_app_list_progressBar);
         progressBar.setVisibility(View.VISIBLE);
 
-        searchEditText = (EditText) findViewById(R.id.app_list_activity_search_edit_text);
-        searchEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                appListAdapter.getFilter().filter(s);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-        searchViewLinearLayout.setVisibility(View.GONE);
         new FetchAppsAsyncTask().execute();
 
     }
@@ -103,7 +94,8 @@ public class AppListActivity extends AppCompatActivity {
         appItem.setAppIcon(applicationInfo.loadIcon(packageManager));
         return appItem;
     }
-    public void onSave(View view)
+
+    public void onSave()
     {
         ArrayList<String> selectedApps = appListAdapter.getSelectedApps();
 
@@ -128,8 +120,92 @@ public class AppListActivity extends AppCompatActivity {
             Log.d(getClass().toString(), "Fetched all apps");
             progressBar.setVisibility(View.GONE);
             recyclerView.setVisibility(View.VISIBLE);
-            searchViewLinearLayout.setVisibility(View.VISIBLE);
         }
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        mSearchAction = menu.findItem(R.id.action_search);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_search:
+                handleMenuSearch();
+                return true;
+            case R.id.app_list_save_button:
+                onSave();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void handleMenuSearch(){
+        ActionBar action = getSupportActionBar(); //get the actionbar
+
+        if(isSearchOpened){ //test if the search is open
+
+
+            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
+            action.setDisplayShowTitleEnabled(true); //show the title in the action bar
+
+            //hides the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+            searchEditText.clearFocus();
+            //add the search icon in the action bar
+            mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_search_black_48dp));
+
+            isSearchOpened = false;
+        } else { //open the search entry
+
+            action.setDisplayShowCustomEnabled(true); //enable it to display a
+            // custom view in the action bar.
+            action.setCustomView(R.layout.app_list_search_bar);//add the custom view
+            action.setDisplayShowTitleEnabled(false); //hide the title
+
+            searchEditText = (EditText) findViewById(R.id.app_list_activity_search_edit_text);
+            searchEditText.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    appListAdapter.getFilter().filter(s);
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+
+
+            searchEditText.requestFocus();
+
+            //open the keyboard focused in the edtSearch
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(searchEditText, InputMethodManager.SHOW_IMPLICIT);
+
+
+            //add the close icon
+            mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_close));
+
+            isSearchOpened = true;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_app_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 }
--- App/FEKC/app/src/main/java/com/example/shiv/fekc/activity/FacebookLoginActivity.java
+++ App/FEKC/app/src/main/java/com/example/shiv/fekc/activity/FacebookLoginActivity.java
@@ -3,8 +3,12 @@ package com.example.shiv.fekc.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -35,6 +39,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 import retrofit.Callback;
@@ -92,6 +98,21 @@ public class FacebookLoginActivity extends AppCompatActivity {
         if (AccessToken.getCurrentAccessToken() != null) {
             Log.d(getClass().toString(), "Credentials present");
             finish();
+        }
+
+        try{
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "com.example.shiv.fekc", PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
         }
     }
 
